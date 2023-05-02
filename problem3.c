7ef7f1c90b1b6940fb6261769cc5411ad5f2d4a0
RyanC528 <123291263+RyanC528@users.noreply.github.com>
2023-05-02 17:30:16 -0400

    Create problem3.c

---
+++ problem3.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   problem1.c
+ * Authors: Ryan Copeland
+ *
+ * Created on May 2, 2023, 5:25 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
