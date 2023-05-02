be418575150b73d8c120b74eec509367c374dd26
DED0TATED <126040465+DED0TATED@users.noreply.github.com>
2023-05-02 19:40:01 -0400

    Update problem2.c

--- problem2.c
+++ problem2.c
@@ -1,18 +1,32 @@
 /* 
  * File:   problem1.c
- * Authors: Ryan Copeland
+ * Authors: Ryan Copeland, Sam Guaetta
  *
  * Created on May 2, 2023, 5:25 PM
  */
 
-#include <stdio.h>
-#include <stdlib.h>
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+#define DOWN        0
+#define UP          1
+#define SWITCH      PORTAbits.RA2
+#define LED         LATCbits.LATC0
 
-    return (EXIT_SUCCESS);
-}
+#include <htc.h>  
 
+void main(void) {
+    int count = 0;              //declare variable to track # of switch presses
+    TRISC = 0;                  //need testing on these two to see what they do I believe it just resets the LED's to off state
+    LATC = 0;
+    
+    TRISAbits.TRISA2 = 1;       //from example: setup switch, switch as input
+    ANSELAbits.ANSA2 = 0;       //digital switch
+    
+    while(1) {
+    if (SWITCH == DOWN) {
+        count += 1;                     //when switch is pressed increase counter by 1
+       if(count >= 16) {
+        count = 0;                      //resets counter to 0 as it exceeds LED binary limits
+       }
+    }
+        
+ //create test cases to match count # with binary number & LED's
