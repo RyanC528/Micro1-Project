31f3e31cb610cce8b45faa579fc05b83d7457db9
RyanC528 <123291263+RyanC528@users.noreply.github.com>
2023-05-03 14:11:45 -0400

    bugfix problem2.c

--- problem2.c
+++ problem2.c
@@ -13,6 +13,8 @@
 
 #include <htc.h>
 #include <stdio.h>
+#define _XTAL_FREQ 500000                               //Used by the XC8 delay_ms(x) macro
+
 
 //config bits that are part-specific for the PIC16F1829
 __CONFIG(FOSC_INTOSC & WDTE_OFF & PWRTE_OFF & MCLRE_OFF & CP_OFF & CPD_OFF & BOREN_ON & CLKOUTEN_OFF & IESO_OFF & FCMEN_OFF);
@@ -26,6 +28,7 @@ __CONFIG(WRT_OFF & PLLEN_OFF & STVREN_OFF & LVP_OFF);
 
 void main(void) {
     TRISC = 0;                  //need testing on these two to see what they do I believe it just resets the LED's to off state
+    OSCCON = 0b00111000;                                //500KHz clock speed
     LATC = 0;
     
     TRISAbits.TRISA2 = 1;       //from example: setup switch, switch as input
@@ -35,62 +38,65 @@ void main(void) {
     
     while (1) {
         if (SWITCH == DOWN) {
-        count += 1;                     //when switch is pressed increase counter by 1
-            if(count >= 16) {
-            count = 0;                      //resets counter to 0 as it exceeds LED binary limits
+            __delay_ms(50);
+            if (SWITCH == UP){
+                count += 1;                     //when switch is pressed increase counter by 1
+                    if(count >= 16) {
+                    count = 0;                      //resets counter to 0 as it exceeds LED binary limits
+                    }
             }
         }
         if (count == 0) {                 // test cases to match count # with binary number & light up LED's accordingly
-            LATC = 0; //0000
+            LATC = 0b0000000; //0000
             }
         if (count == 1) {
-            LATC = 0; //0001
+            LATC = 0b0000001; //0001
             }
         if (count == 2) {
-           LATC = 0; //0010
+           LATC = 0b0000010; //0010
             }
         if (count == 3) {
-            LATC = 0; //0011
+            LATC = 0b0000011; //0011
             }
         if (count == 4) {
-            LATC = 0; //0100
+            LATC = 0b0000100; //0100
             }
         if (count == 5) {
-            LATC = 0; //0101
+            LATC = 0b0000101; //0101
             }
         if (count == 6) {
-            LATC = 0; //0110
+            LATC = 0b0000110; //0110
             }
         if (count == 7) {
-            LATC = 0; //0111
+            LATC = 0b0000111; //0111
             }
         if (count == 8) {
-            LATC = 0; //1000
+            LATC = 0b0001000; //1000
             }
         if (count == 9) {
-            LATC = 0; //01001
+            LATC = 0b0001001; //01001
             }
         if (count == 10) {
-            LATC = 0; //1010
+            LATC = 0b0001010; //1010
             }
         if (count == 11) {
-            LATC = 0; //1011
+            LATC = 0b0001011; //1011
             }
         if (count == 12) {
-            LATC = 0; //1100
+            LATC = 0b0001100; //1100
             }
         if (count == 13) {
-            LATC = 0; //1101
+            LATC = 0b0001101; //1101
             }
         if (count == 14) {
-            LATC = 0; //1110
+            LATC = 0b0001110; //1110
             }
         if (count == 15) {
-            LATC = 0; //1111
+            LATC = 0b0001111; //1111
             }
         if(count >= 16) {
             count = 0;                     //resets counter to 0 as it exceeds LED binary limits
-            LATC = 0; //0000
+            LATC = 0b0000000; //0000
             }
     }
 }
