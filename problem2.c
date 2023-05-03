fa5f46b97ea555e98e7d43b7dbb0eb04b6fc422f
DED0TATED <126040465+DED0TATED@users.noreply.github.com>
2023-05-02 21:17:35 -0400

    Update problem2.c

--- problem2.c
+++ problem2.c
@@ -24,20 +24,18 @@ __CONFIG(WRT_OFF & PLLEN_OFF & STVREN_OFF & LVP_OFF);
      */
 
 void main(void) {
-    int count = 0;              //declare variable to track # of switch presses
     TRISC = 0;                  //need testing on these two to see what they do I believe it just resets the LED's to off state
     LATC = 0;
     
     TRISAbits.TRISA2 = 1;       //from example: setup switch, switch as input
     ANSELAbits.ANSA2 = 0;       //digital switch
     
+    int count = 0;              //declare variable to track # of switch presses
+    
     while (1) {
         if (SWITCH == DOWN) {
         count += 1;                     //when switch is pressed increase counter by 1
-            if(count >= 16) {
-            count = 0;                      //resets counter to 0 as it exceeds LED binary limits
             }
-        }
         if (count == 0) {                 // test cases to match count # with binary number & light up LED's accordingly
             LATC = 0; //0000
             }
@@ -85,5 +83,9 @@ void main(void) {
             }
         if (count = 15) {
             LATC = ?; //1111
+            }
+        if(count >= 16) {
+            count = 0;                     //resets counter to 0 as it exceeds LED binary limits
+            LATC = 0; //0000
             }
     }
