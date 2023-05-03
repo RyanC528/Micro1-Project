db51f90b4d23b5e64963de4e3caa509d77462341
DED0TATED <126040465+DED0TATED@users.noreply.github.com>
2023-05-02 21:01:00 -0400

    Update problem2.c

--- problem2.c
+++ problem2.c
@@ -31,13 +31,59 @@ void main(void) {
     TRISAbits.TRISA2 = 1;       //from example: setup switch, switch as input
     ANSELAbits.ANSA2 = 0;       //digital switch
     
-    while(1) {
-    if (SWITCH == DOWN) {
+    while (1) {
+        if (SWITCH == DOWN) {
         count += 1;                     //when switch is pressed increase counter by 1
-       if(count >= 16) {
-        count = 0;                      //resets counter to 0 as it exceeds LED binary limits
-       }
+            if(count >= 16) {
+            count = 0;                      //resets counter to 0 as it exceeds LED binary limits
+            }
+        }
+        if (count == 0) {                 // test cases to match count # with binary number & light up LED's accordingly
+            LATC = 0; //0000
+            }
+        if (count == 1) {
+            LATC = ?; //0001
+            }
+        if (count == 2) {
+           LATC = ?; //0010
+            }
+        if (count == 3) {
+            LATC = ?; //0011
+            }
+        if (count == 4) {
+            LATC = ?; //0100
+            }
+        if (count == 5) {
+            LATC = ?; //0101
+            }
+        if (count = 6) {
+            LATC = ?; //0110
+            }
+        if (count = 7) {
+            LATC = ?; //0111
+            }
+        if (count = 8) {
+            LATC = ?; //1000
+            }
+        if (count = 9) {
+            LATC = ?; //01001
+            }
+        if (count = 10) {
+            LATC = ?; //1010
+            }
+        if (count = 11) {
+            LATC = ?; //1011
+            }
+        if (count = 12) {
+            LATC = ?; //1100
+            }
+        if (count = 13) {
+            LATC = ?; //1101
+            }
+        if (count = 14) {
+            LATC = ?; //1110
+            }
+        if (count = 15) {
+            LATC = ?; //1111
+            }
     }
-    }
-        
- //create test cases to match count # with binary number & LED's
