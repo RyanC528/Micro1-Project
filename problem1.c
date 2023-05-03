8369e0312efbaa6a44a4f3ff92d485e4232d6ae3
RyanC528 <123291263+RyanC528@users.noreply.github.com>
2023-05-02 20:34:10 -0400

    Update problem1.c

--- problem1.c
+++ problem1.c
@@ -1,17 +1,59 @@
-/* 
- * File:   problem1.c
- * Authors: Ryan Copeland
+/**
+ *******************************************************************
+ * Lesson 3 - "Rotate"
  *
- * Created on May 2, 2023, 5:25 PM
+ * This lesson will introduce shifting instructions as well as bit-oriented skip operations to
+ * move the LED display.
+ *
+ * LEDs rotate from right to left at a rate of 1.5s
+ *
+ * PIC: 16F1829
+ * Compiler: XC8 v1.00
+ * IDE: MPLABX v1.10
+ *
+ * Board: PICkit 3 Low Pin Count Demo Board
+ * Date: 6.1.2012
+ *
+ * *******************************************************************
+ * See Low Pin Count Demo Board User's Guide for Lesson Information*
+ * ******************************************************************
  */
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <htc.h>                                        //PIC hardware mapping
+#define _XTAL_FREQ 500000                               //Used by the XC8 delay_ms(x) macro
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+//config bits that are part-specific for the PIC16F1829
+__CONFIG(FOSC_INTOSC & WDTE_OFF & PWRTE_OFF & MCLRE_OFF & CP_OFF & CPD_OFF & BOREN_ON & CLKOUTEN_OFF & IESO_OFF & FCMEN_OFF);
+__CONFIG(WRT_OFF & PLLEN_OFF & STVREN_OFF & LVP_OFF);
 
-    return (EXIT_SUCCESS);
+    /* -------------------LATC-----------------
+     * Bit#:  -7---6---5---4---3---2---1---0---
+     * LED:   ---------------|DS4|DS3|DS2|DS1|-
+     *-----------------------------------------
+     */
+
+void main(void) {
+    TRISC = 0;                                          //all pins are outputs
+    OSCCON = 0b00111000;                                //500KHz clock speed
+    LATC = 0b0001000;                                   //start the rotation by setting DS4 ON - rotate from the right to left
+
+    while (1) {
+            __delay_ms(1000);                            //delay 1000ms
+            LATC = 0b0001100;                                //shift to the right by 1
+            __delay_ms(1000);                            //delay 1000ms
+            LATC = 0b0001000;
+            __delay_ms(1000);
+            LATC = 0b0001110;
+            __delay_ms(1000);                            //delay 1000ms
+            LATC = 0b0000001;
+             __delay_ms(1000);                            //delay 1000ms
+            LATC = 0b0001001;
+            __delay_ms(1000);                            //delay 1000ms
+            LATC = 0b000110;
+            __delay_ms(1000);                            //delay 1000ms
+            LATC = 0b0001010;
+            __delay_ms(1000);                            //delay 1000ms
+            LATC = 0b0000101;
+    }
+
 }
