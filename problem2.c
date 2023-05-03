0d534b5fd9efbe082c053f4e281c6ea9a8fa1d05
DED0TATED <126040465+DED0TATED@users.noreply.github.com>
2023-05-02 20:35:24 -0400

    Update problem2.c

--- problem2.c
+++ problem2.c
@@ -13,6 +13,16 @@
 
 #include <htc.h>  
 
+//config bits that are part-specific for the PIC16F1829
+__CONFIG(FOSC_INTOSC & WDTE_OFF & PWRTE_OFF & MCLRE_OFF & CP_OFF & CPD_OFF & BOREN_ON & CLKOUTEN_OFF & IESO_OFF & FCMEN_OFF);
+__CONFIG(WRT_OFF & PLLEN_OFF & STVREN_OFF & LVP_OFF);
+
+    /* -------------------LATC-----------------
+     * Bit#:  -7---6---5---4---3---2---1---0---
+     * LED:   ---------------|DS4|DS3|DS2|DS1|-
+     *-----------------------------------------
+     */
+
 void main(void) {
     int count = 0;              //declare variable to track # of switch presses
     TRISC = 0;                  //need testing on these two to see what they do I believe it just resets the LED's to off state
@@ -27,6 +37,7 @@ void main(void) {
        if(count >= 16) {
         count = 0;                      //resets counter to 0 as it exceeds LED binary limits
        }
+    }
     }
         
  //create test cases to match count # with binary number & LED's
